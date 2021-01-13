da14e2fa2a959322121f5a62c51db210631f32cd
Felipe Melo <ftiagomelo89@gmail.com>
2021-01-13 11:02:01 -0300

    Adicao de repository e CRUD para Cliente

--- src/main/java/com/felipemelo/osworks/api/controller/ClienteController.java
+++ src/main/java/com/felipemelo/osworks/api/controller/ClienteController.java
@@ -1,25 +1,76 @@
 package com.felipemelo.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipemelo.osworks.domain.model.Cliente;
+import com.felipemelo.osworks.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
-	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class).getResultList();
+	@GetMapping
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> find(@PathVariable Long id){
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente insert(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> update(@PathVariable Long id, @RequestBody Cliente cliente){
+		
+		if (!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(id);
+		cliente = clienteRepository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		
+		if (!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clienteRepository.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
---
+++ src/main/java/com/felipemelo/osworks/domain/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.felipemelo.osworks.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.felipemelo.osworks.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
