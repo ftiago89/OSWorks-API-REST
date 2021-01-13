b9fd353a4a49bd72a0442de654ac4bcef7f6e762
Felipe Melo <ftiagomelo89@gmail.com>
2021-01-13 10:10:34 -0300

    Adicao e configuracao do Myql server, criacao e teste de tabela Cliente

--- src/main/java/com/felipemelo/osworks/api/controller/ClienteController.java
+++ src/main/java/com/felipemelo/osworks/api/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.felipemelo.osworks.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,21 +14,12 @@ import com.felipemelo.osworks.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setTelefone("83 99999-1111");
-		cliente1.setEmail("joaodascouves@gmail.com");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("83 77777-1111");
-		cliente2.setEmail("mariadasilva@gmail.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 
 }
